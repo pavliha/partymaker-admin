4a1e50ca98ef82c4d53c13fa9192ac66b00dbcaa
Pavel Kostyuk <kostyk.pavel.09@gmail.com>
2019-10-06 14:30:34 +0300

    add entertainments dropdown

--- src/components/DropdownField.jsx
+++ src/components/DropdownField.jsx
@@ -2,26 +2,33 @@ import React, { useCallback } from 'react'
 import { any, arrayOf, bool, func, oneOf, shape, string } from 'prop-types'
 import { FormHelperText, MenuItem, Select, FormControl, InputLabel } from '@material-ui/core'
 
-const DropdownField = ({ items, label, name, value, variant, nonEmpty, error, helperText, onChange }) => {
-  return (
-    <FormControl fullWidth variant={variant}>
-      <InputLabel htmlFor="outlined-age-simple">{label}</InputLabel>
-      <Select
-        value={value || ''}
-        displayEmpty
-        onChange={useCallback((e) => onChange(name, e.target.value))}
-      >
-        {!nonEmpty && <MenuItem value="" />}
-        {items.map((item, index) => (
-          <MenuItem key={index} value={item.value}>
-            {item.label}
-          </MenuItem>
-        ))}
-      </Select>
-      <FormHelperText error={error}>{helperText}</FormHelperText>
-    </FormControl>
-  )
-}
+const DropdownField = ({
+  items,
+  label,
+  name,
+  value,
+  variant,
+  nonEmpty,
+  error,
+  helperText,
+  onChange
+}) =>
+  <FormControl fullWidth variant={variant}>
+    <InputLabel htmlFor="outlined-age-simple">{label}</InputLabel>
+    <Select
+      value={value || ''}
+      displayEmpty
+      onChange={useCallback((e) => onChange(name, e.target.value))}
+    >
+      {!nonEmpty && <MenuItem value="" />}
+      {items.map((item, index) => (
+        <MenuItem key={index} value={item.value}>
+          {item.label}
+        </MenuItem>
+      ))}
+    </Select>
+    <FormHelperText error={error}>{helperText}</FormHelperText>
+  </FormControl>
 
 DropdownField.propTypes = {
   name: string.isRequired,
