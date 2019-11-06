bf6c8ea8bda784a27e25489a8859e44ccae70f74
Pavel Kostyuk <kostyk.pavel.09@gmail.com>
2019-11-06 20:19:06 +0200

    add mysql build status

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 git reset --hard origin/master
 git pull origin master
 sudo service mysql stop
+echo 'Stop mysql service'
 yarn install
 yarn build
 sudo service mysql start
+echo 'Start mysql service'
