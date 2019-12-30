25a1319609a1d52a4c00727517e0bddc5babfb2f
Pavel Kostyuk <kostyk.pavel.09@gmail.com>
2019-12-30 22:03:19 +0200

    refactor Account tests

--- src/components/Account/Account.test.js
+++ src/components/Account/Account.test.js
@@ -1,53 +1,39 @@
 import React from 'react'
 import Account from './Account'
-import renderer from 'react-test-renderer'
 import configureStore from 'redux-mock-store'
 import { Provider } from 'react-redux'
 
-describe('Account', () => {
-  it('renders null when unauthorized', () => {
+describe('<Account />', () => {
 
+  const setup = (Component, state) => {
     const mockStore = configureStore([])
-    const store = mockStore({
-      auth: { user_id: null },
-      users: {
-        entities: {}
-      }
-    })
+    const store = mockStore(state)
 
-    const tree = renderer
-      .create(
-        <Provider store={store}>
-          <Account />
-        </Provider>
-      )
-      .toJSON()
-    expect(tree).toMatchSnapshot()
+    const tree = mount(
+      <Provider store={store}>
+        {Component}
+      </Provider>
+    )
+
+    return tree
+  }
+
+  it('renders null when unauthorized', () => {
+    const state = {
+      auth: { user_id: null },
+      users: { entities: {} }
+    }
+    const tree = setup(<Account />, state)
+    expect(toJson(tree)).toMatchSnapshot()
   })
 
   it('renders username when authorized ', () => {
-
-    const mockStore = configureStore([])
-    const store = mockStore({
+    const state = {
       auth: { user_id: 1 },
-      users: {
-        entities: {
-          1: {
-            id: 1,
-            name: 'Test User'
-          }
-        }
-      }
-    })
-
-    const tree = renderer
-      .create(
-        <Provider store={store}>
-          <Account />
-        </Provider>
-      )
-      .toJSON()
-    expect(tree).toMatchSnapshot()
+      users: { entities: { 1: { id: 1, name: 'Test User' } } }
+    }
+    const tree = setup(<Account />, state)
+    expect(toJson(tree)).toMatchSnapshot()
   })
 
 })
