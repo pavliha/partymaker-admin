d235a19d56af92eb9bb64d70854c42601fc6d239
Pavel Kostyuk <kostyk.pavel.09@gmail.com>
2019-12-31 00:21:10 +0200

    add tests for Field.jsx

--- src/components/Field/Field.test.jsx
+++ src/components/Field/Field.test.jsx
@@ -1,15 +1,44 @@
+import React from 'react'
+import { Form, Formik } from 'formik'
+import Field from './Field'
+import { TextField } from '@material-ui/core'
+
 describe('<Field />', () => {
 
+  const setup = (Component) =>
+    mount(
+      <Formik onSubmit={() => null} initialValues={{ test: 'field' }}>
+        <Form>
+          {Component}
+        </Form>
+      </Formik>
+    )
+
   it('should pass props to field component', () => {
-    // const componentFn = jest.fn()
-    // mount(
-    //   <Formik onSubmit={() => {}} initialValues={{ test: 'field' }}>
-    //     <Form>
-    //       <Field name="test" component={componentFn} />
-    //     </Form>
-    //   </Formik>
-    // )
-    // expect(componentFn).toBeCalledWith({ name: 'test' })
+    const componentFn = jest.fn(() => null)
+
+    setup(<Field name="test" component={componentFn} />)
+
+    expect(componentFn).toBeCalledWith(expect.objectContaining({
+      error: false,
+      fullWidth: true,
+      helperText: undefined,
+      name: 'test',
+      onChange: expect.anything(),
+      onError: expect.anything(),
+      value: 'field',
+    }), expect.anything())
+  })
+
+  it('should handle onChange', () => {
+    const changeFn = jest.fn()
+
+    const tree = setup(<Field name="test" component={TextField} onChange={changeFn} />)
+    const textInput = tree.find(TextField).find('input')
+    const event = { target: { value: 'hello' } }
+    textInput.simulate('change', event)
+    const form = expect.anything()
+    expect(changeFn).toBeCalledWith('test', 'hello', form)
   })
 
 })
