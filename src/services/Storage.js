c0c382fc75d267367a3dbe774bb3a145cbf8d359
Pavel Kostyuk <kostyk.pavel.09@gmail.com>
2019-12-30 22:03:31 +0200

    update Storage.js

--- src/services/Storage.js
+++ src/services/Storage.js
@@ -1,27 +1,21 @@
-class Storage {
+
+const storage = {
 
   get(item) {
-    if (typeof window === 'undefined') return null
-
     return JSON.parse(localStorage.getItem(item))
-  }
+  },
 
   put(items) {
-    if (typeof window === 'undefined') return
-
     const entries = Object.entries(items)
-
     entries.forEach(([key, value]) => {
       localStorage.setItem(key, JSON.stringify(value))
     })
-  }
+  },
 
   clear() {
     localStorage.clear()
   }
 
 }
-
-const storage = new Storage()
 
 export default storage
