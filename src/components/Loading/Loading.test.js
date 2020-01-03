facf1ef3975259da345295e72536cc73d69e3063
Pavel Kostyuk <kostyk.pavel.09@gmail.com>
2020-01-03 19:00:12 +0200

    add more tests for Loading.jsx

--- src/components/Loading/Loading.test.js
+++ src/components/Loading/Loading.test.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import Loading from './Loading'
+import { CircularProgress } from '@material-ui/core'
 
 describe('<Loading />', () => {
 
-  it('should render properly', () => {
-    const tree = mount(<Loading />)
-    expect(toJson(tree)).toMatchSnapshot()
+  it('should be invisible before timeout', () => {
+    const tree = shallow(<Loading />).dive()
+    tree.setState({ isVisible: false })
+    expect(tree.find(CircularProgress).exists()).toBeFalsy()
+  })
+
+  it('should show progress after timeout', () => {
+    const tree = shallow(<Loading />).dive()
+    tree.setState({ isVisible: true })
+    expect(tree.find(CircularProgress).exists()).toBeTruthy()
+  })
+
+  it('should show children when loaded', () => {
+    const tree = shallow(<Loading loaded>child</Loading>).dive()
+    tree.setState({ isVisible: true })
+    expect(tree.text()).toContain('child')
   })
 
 })
