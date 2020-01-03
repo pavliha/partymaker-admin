3ada25a9ec6c5880cf109a093facee3ebce32af4
Pavel Kostyuk <kostyk.pavel.09@gmail.com>
2020-01-03 18:52:29 +0200

    add more tests for Loader.jsx

--- src/components/Loader/Loader.test.js
+++ src/components/Loader/Loader.test.js
@@ -1,6 +1,8 @@
+/* eslint-disable prefer-promise-reject-errors */
 import React from 'react'
 import Loader from './Loader'
-import wait from 'utils/wait'
+import { wait } from 'utils'
+import ErrorIcon from 'mdi-react/ErrorIcon'
 
 describe('<Loader />', () => {
 
@@ -12,6 +14,19 @@ describe('<Loader />', () => {
   it('should display content', async () => {
     const tree = shallow(<Loader {...testProps} load={() => wait(10)} />).dive()
     await expect(tree.text()).toContain('dummy')
+  })
+
+  it('should pass params to load prop', async () => {
+    const waitFn = jest.fn()
+    mount(<Loader {...testProps} load={waitFn} params={10} />)
+    await expect(waitFn).toBeCalledWith(10)
+  })
+
+  it('should display error', async () => {
+    const waitFn = jest.fn(() => Promise.reject('error'))
+    const loader = shallow(<Loader {...testProps} load={waitFn} params={10} />).dive()
+    loader.setState({ error: true })
+    setTimeout(expect(loader.find(ErrorIcon).exists()).toBeTruthy())
   })
 
 })
